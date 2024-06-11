666bc78464590d0d1761c98ffe317059cf188161
han6054 <424681370@qq.com>
2024-06-11 10:11:12 +0800

    first commit

---
+++ signup/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import signupAction from './signupAction'
+export default function Signup() {
+  const [error, formAction] = useFormState(signupAction, undefined);
+
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form action={formAction}>
+        <input type="email" name="email" />
+        <input type="password" name="password" />
+        <button type="submit">Signup</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+}
